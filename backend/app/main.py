04b2101fb63131fda58d0acc7d094dd6d98fc189
MarquesAnt <149375410+MarquesAnt@users.noreply.github.com>
2026-01-06 11:43:22 +0100

    chore : project setup and gitignore

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"status": "Cine-Companion Backend Online"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
